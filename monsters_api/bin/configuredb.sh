47dbd0de2956c5fff851ab2e12dd222e452be01c
Stav Davis <stav.davis@gmail.com>
2018-05-02 15:06:26 -0700

    changed configuredb.sh to work on ubuntu (commented out mac version lines)

--- monsters_api/bin/configuredb.sh
+++ monsters_api/bin/configuredb.sh
@@ -2,14 +2,30 @@
 # Note1: Use "npm run configure" to run this (see package.json script)
 # Note2: Don't forget to add this file to gitignore
 # The next line creates an env var that feeds the passowrd prompts
+
 export PGPASSWORD='a'
 
 database="monstersdb"
 echo "Configuring database: $database"
 
-dropdb -U node_user monstersdb  #will hang if no db or if db is empty
-createdb -U node_user monstersdb
+sudo -u node_user dropdb monstersdb #this works on ubuntu
+#dropdb -U node_user monstersdb  #Mac version
 
-psql -U node_user monstersdb < ./bin/sql/monsters.sql
+sudo -u node_user createdb monstersdb #this works on ubuntu
+#createdb -U node_user monstersdb #Mac version
+
+sudo -u node_user psql monstersdb < ./bin/sql/monsters.sql
+#psql -U node_user monstersdb < ./bin/sql/monsters.sql #Mac version
+
+echo "$database configured"
+
+export PGPASSWORD='a'
+
+database="monstersdb"
+echo "Configuring database: $database"
+
+
+
+
 
 echo "$database configured"
